673b861dcea7d9988a304ad7eb9612ba3ae297cf
ilib0x00000000 <314149474@qq.com>
2016-05-10 21:42:01 +0800

    日期时间模块

--- python cookbook/datetime_demo.py
+++ python cookbook/datetime_demo.py
@@ -6,6 +6,19 @@
 与时间有关的标准库：
 	o time模块
 	o datetime模块
+
+常用的函数：
+	1.获得当前时间戳
+		time.time()
+		
+	2.获得当前格式化日期时间字符串
+		time.ctime()
+
+	3.获得当前日期时间对象
+		datetime.datetime.now()
+
+	4.把字符串转换成日期时间对象返回
+		datetime.datetime.strptime(date_string, format)
 """
 
 import time
@@ -15,7 +28,7 @@ import datetime
 
 def test_time():
 	"""time模块:
-						时间戳<------------>	格式化时间字符串<------------>时间结构体
+				时间戳<------------>	格式化时间字符串<------------>时间结构体
 						
 		o time(): 		返回自纪元以来经过的秒数
 		o ctime([time]):如果给定一个秒数，就返回该秒数的格式化时间字符串，否则就返回当前的格式化时间字符串
@@ -43,8 +56,42 @@ def test_time():
 
 	print '字符串时间------>时间结构体:', time.strptime('2016-05-09 22:25:18', '%Y-%m-%d %H:%M:%S')
 	print '时间结构体------>字符串时间:', time.strftime('%Y-%m-%d %H:%M:%S')
+	print 
 
 
+
+def test_date():
+	"""datetime模块中的date类
+			属性：
+				o year: 年
+				o month: 月
+				o day: 日
+
+			方法：
+				o ctime(): 返回格式化的日期时间字符串
+				o weekday(): 返回当前日期是一周的第几天
+				o timetuple():返回当前日期的结构体
+
+			类方法：
+				o today(): 返回今天的日期字符串
+				o fromtimestamp(): 把时间戳转换成日期
+	"""
+	ts = time.time()
+	print datetime.date.fromtimestamp(ts)
+
+
+	# 日期对象的运算
+	# 1.创建一个timedelta对象
+	tdl = datetime.timedelta(days = 1)
+	today = datetime.date.today()
+	print today
+
+	# 2.运算
+	yesterday = today - tdl
+	tomorrow = today + tdl
+	print 'yesterday: ', yesterday
+	print 'tomorrow: ', tomorrow
+	print 
 
 
 def test_datetime():
@@ -54,18 +101,6 @@ def test_datetime():
 			o minute:分钟
 			o second:秒数
 
-		date类：
-			属性：
-				o year: 年
-				o month: 月
-				o day: 日
-
-			方法：
-				o ctime(): 返回格式化的日期时间字符串
-				o today(): 返回今天的日期字符串
-				o weekday(): 返回当前日期是一周的第几天
-				o timetuple():返回当前日期的结构体
-
 		datetime类：
 			属性：包含time和date类的属性
 			方法：包含time和date类的方法
@@ -73,16 +108,23 @@ def test_datetime():
 	tm = datetime.time(1, 2, 3)
 	print tm.hour, '\t', tm.minute, '\t', tm.second
 
-	dt = datetime.date(2016, 05, 09)
-	print dt.year, dt.month, dt.day
-	print dt.ctime()
-	print dt.today()
-	print dt.weekday()
-	print dt.timetuple()
 
+	print datetime.datetime.now()		# 返回现在的日期时间
+	print datetime.datetime.today()		# 同上
 
+	# 格式化与解析
+	# 1.把日期时间对象转换成字符串
+	s = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+	print s
+	print type(s)
 
+	# 2.把字符串转换成日期时间对象
+	dt = datetime.datetime.strptime('2015-5-10 21:27:35', '%Y-%m-%d %H:%M:%S')
+	print dt
+	print type(dt)
 
 
 if __name__ == '__main__':
 	test_time()
+	test_date()
+	test_datetime()
