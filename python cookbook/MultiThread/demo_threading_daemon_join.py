fc63c10f04eae1b44b04389a3239129a2cdd546a
ilib0x00000000 <314149474@qq.com>
2016-05-25 22:32:52 +0800

    守护线程

---
+++ python cookbook/MultiThread/demo_threading_daemon_join.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env  python
+# -*- coding:utf-8 -*-
+
+import time
+import logging
+import threading
+
+# 配置日志
+logging.basicConfig(level=logging.DEBUG, format='(%(threadName)s) %(message)s')
+
+
+def daemon():
+	logging.debug('Starting')
+	time.sleep(2)
+	logging.debug('Exiting')
+
+def non_daemon():
+	logging.debug('Starting')
+	logging.debug('Exiting')
+
+d = threading.Thread(name='daemon', target=daemon)
+d.setDaemon(True)
+
+t = threading.Thread(name='non_daemon', target=non_daemon)
+
+d.start()
+t.start()
+
+d.join()
+t.join()
+
+# output
+# (daemon) Starting
+# (non_daemon) Starting
+# (non_daemon) Exiting
+# (daemon) Exiting
